bf56a1d93568883e7cafcc7ca48ac602dbeaa13f
lucassilva18byte <lucas.santossilva1906@gmail.com>
2026-02-26 18:17:55 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home () {
 
-    return <h1>Amor, eu amo você. Se você me ama, dá um risadinha</h1>
+    return <h1>Amor, eu amo você. Se você me ama, dá um risadinha!♥ </h1>
 
 }
 export default Home; 
